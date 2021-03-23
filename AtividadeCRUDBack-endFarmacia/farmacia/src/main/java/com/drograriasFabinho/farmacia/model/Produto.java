64efcda1303cda36f727f4888d878e9db55eb296
unknown <fabiolimadiogenes@gmail.com>
2021-03-23 14:37:48 -0300

    Enviando atividade criando CRUD Farmacia

---
+++ AtividadeCRUDBack-endFarmacia/farmacia/src/main/java/com/drograriasFabinho/farmacia/model/Produto.java
@@ -0,0 +1,74 @@
+package com.drograriasFabinho.farmacia.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name = "tb_produto")
+public class Produto {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+
+	@NotNull
+	@Size(min = 3, max = 50)
+	private String nome;
+	
+	@NotNull
+	@Size(min = 5, max = 100)
+	private String descricao;
+	
+	@NotNull
+	private int quantidade;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Categoria categoria;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public int getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+		
+}
